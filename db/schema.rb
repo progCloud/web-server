7008253f6ca09dfd180e1e3e48b65c82c6123d70
peeyush <peeyushagarwal1994@gmail.com>
2014-10-31 15:35:00 +0530

    Use MySQL on server instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20141016145444) do
     t.datetime "uploaded_file_updated_at"
   end
 
-  add_index "assets", ["user_id"], name: "index_assets_on_user_id"
+  add_index "assets", ["user_id"], name: "index_assets_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20141016145444) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
